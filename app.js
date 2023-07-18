d93a7b7a8ac4db5b6dc407571f027f0f51fac3a6
Raafatbaki <raafat.baki.93@gmail.com>
2023-07-18 17:49:05 +0200

    commit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { error } = require('console');
 
 var app = express();
 
@@ -21,6 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 
+mongoose.connect('mongodb://127.0.0.1:27017/Shopping-cart', {useNewUrlParser: true})
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  // mongoose.connect('mongodb://127.0.0.1:27017/Shopping-cart', {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // })
+  //   .then(() => {
+  //     console.log('Connected to MongoDB');
+  //     // استمر في تنفيذ الخطوات الأخرى بعد الاتصال بنجاح
+  //   })
+  //   .catch((error) => {
+  //     console.error('Failed to connect to MongoDB:', error);
+  //   });
+
+  // mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true })
+  // .then(() => {
+  //   // إنشاء قاعدة البيانات "Shopping-cart"
+  //   return mongoose.connection.db.createDatabase('Shopping-cart');
+  // })
+  // .then(() => {
+  //   console.log('#');
+  //   // استمر في تنفيذ الخطوات الأخرى بعد إنشاء قاعدة البيانات
+  // })
+  // .catch((error) => {
+  //   console.log('f', error);
+  // });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
