91ec2adf2456bc98d165d61790addaadaeb2eb1e
Raafatbaki <raafat.baki.93@gmail.com>
2023-07-19 19:52:43 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shopping Cart' });
+  Product.find({})
+  .then((docs) => {
+    let productGrid = [];
+    let colGrid = 3;
+
+    for (let i = 0; i < docs.length; i += colGrid) {
+      productGrid.push(docs.slice(i, i + colGrid))
+    }
+
+    res.render('index', {
+      title: 'Shopping-cart',
+      products: productGrid
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 });
 
 module.exports = router;
