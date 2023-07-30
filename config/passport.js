acfb0aa5ce4107390ed6aad7e0afa93be3306f3c
Raafatbaki <raafat.baki.93@gmail.com>
2023-07-30 19:43:34 +0200

    commit

--- config/passport.js
+++ config/passport.js
@@ -4,35 +4,27 @@ const User = require("../models/user");
 
 
 passport.use("local-signIn", new localStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    (req, email, password, done) => {
-      User.find({ email: email })
-        .then((user) => {
-          if (!user) {
-            return done(null,false,req.flash("signInError", "this user not found"));
-          }
+  {
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true,
+  },
+  (req, email, password, done) => {
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, req.flash("signInError", "this user not found"));
+        }
 
-          return user.comparePassword(password)
-            .then((isMatch) => {
-              if (!isMatch) {
-                return done(null,false,req.flash("signinError", "wrong password"));
-              }
-              return done(null, user);
-            })
-            .catch((err) => {
-              return done(err);
-            });
-        })
-
-        .catch((err) => {
-          return done(err);
-        });
-    }
-  )
-);
+        if (!user.comparePassword(password)) {
+          return done(null, false, req.flash("signinError", "wrong password"));
+        }
+        return done(null, user);
+      })
+      .catch((err) => {
+        return done(err);
+      });
+  }
+));
 
 
