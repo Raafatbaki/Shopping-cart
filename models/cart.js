0a3183c087462077ebe635206e94d8d8e4c5b519
Raafatbaki <raafat.baki.93@gmail.com>
2023-08-02 17:56:34 +0200

    commit

---
+++ models/cart.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const cartSchema = mongoose.Schema({
+  _id: {
+    required: true,
+    type: String,
+  },
+
+  totalquantity: {
+    required: true,
+    type: Number,
+  },
+
+  totalPrice: {
+    required: true,
+    type: Number,
+  },
+
+  selectedProduct: {
+    required: true,
+    type: Array,
+  },
+});
+
+module.exports = mongoose.model("cart", cartSchema);
